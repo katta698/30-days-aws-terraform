6ff47ec6277af43cc04889c0a8b80a7f878857e3
katta698 <katta.jayant@gmail.com>
2026-05-16 00:10:36 -0500

    Updated backend s3

--- day-28 - 3-Tier AWS Infrastructure/terraform/backend.tf
+++ day-28 - 3-Tier AWS Infrastructure/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "jay-terraformstate-bucket"
-    key          = "day-28/dev/terraform.tfstate"
+    key          = "day-28v2/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
